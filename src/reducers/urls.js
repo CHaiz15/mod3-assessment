cd0280ea330ce7840cededcfafa3ed2922e0e8be
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-04 14:01:30 -0700

    Add DELETE_URL case

--- src/reducers/urls.js
+++ src/reducers/urls.js
@@ -4,6 +4,9 @@ export const urlsReducer = (state = [], action) => {
       return action.urls;
     case 'ADD_URL':
         return [...state, action.url];
+    case 'DELETE_URL':
+        const filteredUrls = state.filter(url => url.id !== action.url.id)
+        return filteredUrls;
     default:
       return state;
   }
