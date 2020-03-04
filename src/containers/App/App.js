635bc00990653e34c3c3902f7d2d4ca73ae05c97
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-03-04 11:53:05 -0700

    Clean up app

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import UrlContainer from '../../components/UrlContainer/UrlContainer';
 import UrlForm from '../../components/UrlForm/UrlForm';
